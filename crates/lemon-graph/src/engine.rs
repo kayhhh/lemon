6de18a0ed56cae1d35e1b38961565d4e6e5f823f
Kayh <basedlainpilled@gmail.com>
2024-02-19 12:25:37 -0500

    add delay tests

--- crates/lemon-graph/src/engine.rs
+++ crates/lemon-graph/src/engine.rs
@@ -39,6 +39,10 @@ pub struct ExecutionStep {
 }
 
 impl ExecutionStep {
+    pub fn new(idx: NodeIndex) -> Self {
+        Self { idx }
+    }
+
     pub async fn step(&self, graph: &mut Graph) -> Vec<ExecutionStep> {
         // Read input from the graph.
         let input = read_input(self.idx, graph);
@@ -62,7 +66,7 @@ impl ExecutionStep {
         graph
             .edges_directed(self.idx, Direction::Outgoing)
             .filter_map(|edge| match &graph[edge.id()] {
-                GraphEdge::Flow => Some(ExecutionStep { idx: edge.target() }),
+                GraphEdge::Flow => Some(ExecutionStep::new(edge.target())),
                 _ => None,
             })
             .collect()
@@ -94,8 +98,8 @@ mod tests {
     async fn test_engine() {
         let mut graph = Graph::new();
 
-        let node_1 = graph.add_node(Delay { duration: 1.0 }.into());
-        let node_2 = graph.add_node(Delay { duration: 0.5 }.into());
+        let node_1 = graph.add_node(Delay::new(0.1).into());
+        let node_2 = graph.add_node(Delay::new(0.2).into());
         graph.add_edge(node_1, node_2, GraphEdge::Flow);
 
         let trigger = graph.add_node(GraphNode::Trigger("start".to_string()));
--- crates/lemon-graph/src/nodes/delay.rs
+++ crates/lemon-graph/src/nodes/delay.rs
@@ -10,6 +10,12 @@ pub struct Delay {
     pub duration: f32,
 }
 
+impl Delay {
+    pub fn new(duration: f32) -> Self {
+        Self { duration }
+    }
+}
+
 impl Node for Delay {
     fn process_input(&mut self, input: HashMap<String, Data>) {
         if let Some(Data::F32(duration)) = input.get("duration") {
@@ -37,5 +43,38 @@ impl SyncNode for Delay {
 impl From<Delay> for GraphNode {
     fn from(node: Delay) -> Self {
         GraphNode::Sync(Box::new(node))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{engine::ExecutionStep, Graph};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_delay_async() {
+        let mut graph = Graph::new();
+        let delay_idx = graph.add_node(Delay::new(0.1).into());
+
+        let start = std::time::Instant::now();
+        let step = ExecutionStep::new(delay_idx);
+        step.step(&mut graph).await;
+        let end = std::time::Instant::now();
+
+        assert!(end - start >= Duration::from_secs_f32(0.1));
+    }
+
+    #[tokio::test]
+    async fn test_delay_sync() {
+        let mut graph = Graph::new();
+        let delay_idx = graph.add_node(Delay::new(0.1).into());
+
+        let start = std::time::Instant::now();
+        let step = ExecutionStep::new(delay_idx);
+        step.step(&mut graph).await;
+        let end = std::time::Instant::now();
+
+        assert!(end - start >= Duration::from_secs_f32(0.1));
     }
 }
